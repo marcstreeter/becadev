9a2ed9ec072853c7777a8dbf9c8bdfa95e640e49
Marc Streeter <mstreeter@gmail.com>
2024-09-27 18:40:55 -0500

    chore: removed pages as they are not supported in static sites using AppRouter

--- app/yeets/[yeet]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function YeetDetail({ params } : { params: { yeet: string}}) {
-    return (
-        <main>
-            <h1>Yeet detail for { params.yeet }</h1>
-        </main>
-    )
-}
