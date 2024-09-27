ad1f3f18f02de57c3fb70c8ba2f5941cee2bced4
Marc Streeter <mstreeter@gmail.com>
2024-09-27 18:38:41 -0500

    chore: change location of where exports to and makde so that is a static site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    distDir: 'docs', // this is the directory I wish to use for github pages and where I store CNAME
+    output: 'export', // this allows for static sites like those used by github pages
+};
 
 export default nextConfig;
