2cde8078ebebb40de51b53ee4435bdc52fe25228
Marc Streeter <mstreeter@gmail.com>
2024-09-28 20:51:11 -0500

    chore: fix for bad config on nextjs assetPrefix

--- app/awesome/page.tsx
+++ app/awesome/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function AwesomePage() {
     return (
         <main>
-            <h1>NextJS is Awesome - but we are so much more awesome. Oh so much more awesome!</h1>
+            <h1>NextJS is Awesome - but we are so much more awesome. Oh so much more awesome! But FO' REAL!!</h1>
             <Box author="Marc" body="He's a developer" />
             <Box author="Dalinda" body="She's a developer" />
         </main>
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     distDir: 'docs', // this is the directory I wish to use for github pages and where I store CNAME
     output: 'export', // this allows for static sites like those used by github pages
-    assetPrefix: 'media', // github pages does not support the default naming schema of nextjs "_static"
+    assetPrefix: '/media', // github pages does not support the default naming schema of nextjs "_static"
 };
 
 export default nextConfig;
