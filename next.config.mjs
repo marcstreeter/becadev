ee2cfc31dbbab8ee7a072dd6091d42b9377ffec6
Marc Streeter <mstreeter@gmail.com>
2024-09-29 23:21:44 -0500

    chore: removed assetPrefix as it was not useful for getting GithubPages to work properly

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     distDir: 'docs', // this is the directory I wish to use for github pages and where I store CNAME
     output: 'export', // this allows for static sites like those used by github pages
-    assetPrefix: '/media', // github pages does not support the default naming schema of nextjs "_static"
+    // the whole point of the assetPrefix was to remove "_next", it does not remove it
+    // solutions from this thread point to middleware which is server side only https://github.com/vercel/next.js/issues/5602
+    // instead using a .nojekyll file per https://github.blog/news-insights/the-library/bypassing-jekyll-on-github-pages/
+    //assetPrefix: '/media', 
 };
 
 export default nextConfig;
