5ad338a04cafd4c07f28cc5ce93a8b2a0741f0a4
Marc Streeter <mstreeter@gmail.com>
2024-09-29 21:58:19 -0500

    chore: updated so that the _next usage goes away

---
+++ app/middleware.ts
@@ -0,0 +1,15 @@
+// references:
+//   https://nextjs.org/docs/pages/building-your-application/routing/middleware#example
+//   https://github.com/vercel/next.js/issues/5602#issuecomment-986915749
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(req: NextRequest): NextResponse | null {
+    if (req.nextUrl.href.includes('/media/_next/')){
+        return NextResponse.rewrite(
+            req.nextUrl.href.replace('/media/_next/', '/media/'),
+        );
+    }
+    return null;
+}
